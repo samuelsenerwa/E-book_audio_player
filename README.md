439f118d0c1bd114e185f44e4b31a00b5d7ff5aa
samuelsenerwa <samuelsenerwa@gmail.com>
2023-07-03 20:28:52 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# e_book_audio_player
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/e_book_audio_player/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.e_book_audio_player
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
